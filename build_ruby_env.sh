1c2c2aead43a89fc6ec42e4358b7d14953dd50a6
Michael Smith <smith11235@gmail.com>
2014-08-16 21:44:44 -0400

    adding ruby logic

---
+++ build_ruby_env.sh
@@ -0,0 +1,5 @@
+export $rvm_path=`pwd`/.rvm
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+source $rvm_path/scripts/rvm
+gem install bundler
+bundle install
---
+++ load_ruby_env.sh
@@ -0,0 +1,2 @@
+export $rvm_path=`pwd`/.rvm
+source $rvm_path/scripts/rvm
