9edfb094ebfdc828123a781b2fc036e8a2031b66
smith11235 <smith11235@gmail.com>
2014-09-23 17:47:17 -0400

    build script

--- build_ruby_env.sh
+++ build_ruby_env.sh
@@ -1,5 +1,21 @@
+# functions and variables
+#unset $(set | awk -F"[= ]" '/^\w*(RUBY|GEM|IRB|rvm|gem|rubies)\w*(=| \(\))/ {print $1}')
+ 
+# aliases
+#unalias $(alias | awk -F"[= ]" '/rvm/ {print $2}')
+ 
+# others
+#unset cd file_exists_at_url popd pushd
+ 
+# PATH
+# export PATH=$(echo $PATH | tr ':' '\n' | grep -v rvm | tr '\n' ':' | sed 's/:$//')
+
+rm -rf .rvm
 export rvm_path=`pwd`/.rvm
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+\curl -sSL https://get.rvm.io | bash -s stable --ruby-1.9.3
 source $rvm_path/scripts/rvm
+which rvm
+rvm install ruby-1.9.3
+rvm use ruby-1.9.3
 gem install bundler
 bundle install
