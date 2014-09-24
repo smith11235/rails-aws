15ddc8699e0544156417740aca77bcf163c9a6fe
smith11235 <smith11235@gmail.com>
2014-09-23 20:01:31 -0400

    adding aws key file generator

---
+++ lib/rails-aws/setup_generator.rb
@@ -0,0 +1,17 @@
+module RailsAWS
+
+	class SetupGenerator < Rails::Generators::Base
+
+		def aws_keys_config_file
+			create_file "config/aws-keys.yml" do
+				keys = %w(access_key secret_access_key)
+				values = Hash.new
+				keys.each do |key|
+					values[key] = ask "What is the '#{key}'?" 
+				end
+				values.to_yaml
+			end
+		end
+	end
+
+end
