7bdae0e3b641c988615e9fc3d346ca5b7351ce95
smith11235 <smith11235@gmail.com>
2014-10-05 19:13:26 -0400

    removing route from root

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 	end
 
   # You can have the root of your site routed with "root"
-  root 'branches#index'
+  # root 'branches#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
