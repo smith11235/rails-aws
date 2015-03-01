9b005e154aa0cf313bdc2013757ad644e222536c
Michael Smith <smith11235@gmail.com>
2015-02-28 19:22:25 -0500

    more specs

--- spec/aws_generator.rb
+++ spec/aws_generator.rb
@@ -12,11 +12,20 @@ describe RailsAws::AwsGenerator do
   end
 
   it "should create a valid config/rails-aws.yml"
-  
-  it "should create an aws iam key file" 
+ 
+  it "should create an aws iam key file" do
+    aws_key_file = "config/aws-keys.yml"
+    expect(File.file?(aws_key_file)).to be_true
+    aws_key = YAML.load_file aws_key_file
+    expect(aws_key.keys.sort).to eq([:access_key_id,:secret_access_key])
+  end
+
   it "should create deploy keys"
 
-  it "should override the config/database.yml file"
-  it "should add private files to .gitignore"
+  it "should add private files to .gitignore" do
+    # %w(config/rails-aws.yml config/rails-aws/ config/aws-keys.yml) 
+  end
+
+  it "v2: should update rails projects with a simple config/database.yml"
 
 end
