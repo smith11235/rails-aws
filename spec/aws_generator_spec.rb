b1f373f6929d7057ecd6cf060f933408d3e43097
Michael Smith <smith11235@gmail.com>
2015-02-28 19:32:55 -0500

    renamed

--- spec/aws_generator_spec.rb
+++ spec/aws_generator_spec.rb
@@ -14,6 +14,14 @@ describe RailsAws::AwsGenerator do
   it "should create a valid config/rails-aws.yml"
  
   it "should create an aws iam key file" do
+    expect(destination_root).to have_structure do
+      directory "config" do
+        file "rails-aws.yml" do
+          contains ":access_key_id: "
+          contains ":secret_access_key: "
+        end
+      end
+    end
     aws_key_file = "config/aws-keys.yml"
     expect(File.file?(aws_key_file)).to be_true
     aws_key = YAML.load_file aws_key_file
