91b9e0ec6c5f6b31ea13161b0e9c68f70db020f2
smith11235 <smith11235@gmail.com>
2015-04-05 19:23:16 +0000

    delete stub

---
+++ lib/rails-aws/stack_builder.rb
@@ -0,0 +1,90 @@
+module RailsAws
+  class StackBuilder
+    include RailsAws
+
+    def initialize
+      @config = RailsAws::Config.new
+    end
+
+    def cloudformation_file
+      "config/aws-stacks/#{@config.current_stack_name}.json"
+    end
+
+    def prepare_new_stack
+      files_dont_exist_yet
+
+      template = base_config
+
+      db_type = @config.branch.fetch('database').fetch('db_type')
+
+      if db_type != 'sqlite'
+        resources = template.fetch("Resources")
+        resources.merge! rds_config
+      end
+
+      aws_config_dir = File.dirname(cloudformation_file) 
+      FileUtils.mkdir_p(aws_config_dir) unless File.directory? aws_config_dir
+      File.open(cloudformation_file, 'w') do |f|
+        f.puts JSON.pretty_generate(template)
+      end
+    end
+
+    private
+
+    def stack_name
+      @config.current_stack_name
+    end
+
+    def base_config
+      content = File.read File.expand_path("../base_stack_config.yml", __FILE__)
+
+      renderer = ERB.new(content, nil, '%')
+      content = renderer.result(binding)
+
+      YAML.load content
+    end
+
+    def files_dont_exist_yet
+      [key_file, cloudformation_file].each do |file|
+        raise t("stack_builder.errors.file_exists_already", file: file) if File.file? file
+      end
+    end
+
+    def key_file
+      "config/aws-stacks/#{@config.current_stack_name}.key"
+    end
+
+    def rds_config
+      {
+        "RDS" => {
+          "Type" => "AWS=>=>RDS=>=>DBInstance",
+          "Properties" =>
+          {
+            "AllocatedStorage" => "50",
+            "AllowMajorVersionUpgrade" => true,
+            "AutoMinorVersionUpgrade" => true,
+            "BackupRetentionPeriod" => "2",
+            "DBInstanceClass" => @config.branch.fetch('database').fetch('instance_type'),
+            "VPCSecurityGroups"=> [ {"Ref"=> "SecurityGroup"} ],
+            "Engine" => @config.branch.fetch('database').fetch('db_type'),
+            "DBName" => "railsapp", 
+            "MasterUsername" => "railsapp",
+            "MasterUserPassword" => "5aed99058d873716ebec7111b2e679dc",
+            "MultiAZ" => false,
+            "PubliclyAccessible" => false,
+            "DBSubnetGroupName"=> { "Ref"=> "RDSSubnet" },
+            "Tags" => [ {"Key"=> "Name", "Value"=> stack_name } ]
+          }
+        },
+        "RDSSubnet"=> {
+          "Type" => "AWS=>=>RDS=>=>DBSubnetGroup",
+          "Properties" => {
+            "DBSubnetGroupDescription" => stack_name,
+            "SubnetIds" => [ {"Ref"=> "subnet" }, { "Ref"=> "subnet2" } ],
+            "Tags" => [ {"Key"=> "Name", "Value"=> stack_name } ]
+          }
+        }
+      }
+    end
+  end
+end
