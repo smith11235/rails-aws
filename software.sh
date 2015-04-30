dd47dfc538afa00da75a7e8374bc890000099de6
smith11235 <smith11235@gmail.com>
2015-04-30 21:04:35 +0000

    configs

--- software.sh
+++ software.sh
@@ -2,24 +2,27 @@ sudo -i wget --no-check-certificate https://raw.githubusercontent.com/bcwik9/Scr
 sudo -i sh setup_dev_server.sh
 
 configure /opt/nginx/conf/nginx.conf
-  server {    
+
+  server {
     listen 80; 
-    server_name smith.brickworksoftware.com;    
+    server_name smith.brickworksoftware.com;
     location / { 
-      proxy_pass http://localhost:3000;      
+      proxy_pass http://localhost:3000;
       proxy_set_header   X-Real-IP $remote_addr;
-      proxy_set_header   Host      $http_host;    
-    }   
+      proxy_set_header   Host      $http_host;
+    }
   }
-  server {    
-    listen 80; 
-    server_name mail.smith.brickworksoftware.com;    
-    location / { 
-      proxy_pass http://localhost:1080;      
+
+  server {
+    listen 80;
+    server_name mail.smith.brickworksoftware.com;
+    location / {
+      proxy_pass http://localhost:1080;
       proxy_set_header   X-Real-IP $remote_addr;
-      proxy_set_header   Host      $http_host;    
-    }   
+      proxy_set_header   Host      $http_host;  
+    }
   }
+
 
 sudo /opt/nginx/sbin/nginx
 
@@ -27,21 +30,31 @@ wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 make
-make install
+sudo make install
 nohup redis-server &> redis-server.log &
+cd
+sudo apt-get ImageMagick libmagickwand-dev phantomjs -y  # a few missing dependencies
+
 
 ssh-keygen -t rsa -C "your_github_email@example.com"
 # copy contents of ~/.ssh/id_rsa.pub
 # add it to your github account ssh keys
-
 git config --global user.name "smith11235"
 git config --global user.email smith11235@gmail.com
-
 ssh -T git@github.com
-
 exit
 
-* install your project
+git clone git@github.com:BrickworkSoftware/brickwork_app.git
+bundle install --deployment
+
+setup your rds params config/application.yml
+
+RDS_PORT: "5432"
+RDS_DB_NAME: railsapp
+RDS_USERNAME: railsapp
+RDS_PASSWORD: 5aed99058d873716ebec7111b2e679dc 
+RDS_HOSTNAME: rd9u5dhkwgiziy.cpk572oso19t.us-east-1.rds.amazonaws.com # changeme
+  bundle exec rake db:setup
 
 tmux new -s brickworks
 > source build/dev-smith.sh
